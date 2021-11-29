f5ff8621b193d99c76aa0e2a4cb36a2a91082f01
Musa2106 <sandisemutrap@gmail.com>
2021-11-29 15:51:03 +0200

    step#1

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("******Timetable******");
+        System.out.println("***acc**** 17:00pm***");
+        System.out.println("***lo   ** 13:00pm***");
+        System.out.println("***maths** 16:00pm");
+    }
+}
